1a9fee01a69420ca90e37ef0a40f486583c03675
binny <90454079+binny3213@users.noreply.github.com>
2024-06-23 16:25:38 +0300

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,34 @@
+import requests
+
+API_KEY = 'fca_live_2O1GeRw5WHpEMQK6srhR0QJPOZms4qgTJ8NJuEzc'
+BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
+
+CURRENCIES = ["USD","CAD","EUR","AUD","CNY","ILS"]
+
+def convert_currency(base):
+    currencies = ",".join(CURRENCIES)
+    url = f"{BASE_URL}&base_currency={base}&currencies={currencies}"
+
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data["data"]
+
+    except:
+        print("Invalid currency. ")
+        return None
+
+
+while True:
+    base = input("Enter the base currency (q for quit): ").upper()
+
+    if base == "Q":
+        break
+
+    data = convert_currency(base)
+    if not data:
+        continue
+
+    del data[base]
+    for ticker, value in data.items():
+        print(f"{ticker}: {value}")
